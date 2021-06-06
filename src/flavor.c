db153162ed7833045191547b98223d1bb66d89f4
sulkasormi <37194102+sulkasormi@users.noreply.github.com>
2021-06-06 04:01:30 +0300

    apply mark_dragon to dragon fangs

--- src/flavor.c
+++ src/flavor.c
@@ -873,7 +873,8 @@ static char *get_ability_abbreviation(char *ptr, object_type *o_ptr, bool all)
     if ( obj_is_identified(o_ptr)
       && (object_is_wearable(o_ptr) || object_is_ammo(o_ptr))
       && ((object_is_artifact(o_ptr)) || (object_is_ego(o_ptr)) ||
-          ((mark_dragon) && (object_is_dragon_armor(o_ptr))))
+          ((mark_dragon) && ((object_is_dragon_armor(o_ptr)) ||
+           (object_is_(o_ptr, TV_SWORD, SV_DRAGON_FANG)))))
       && !obj_is_identified_fully(o_ptr)
       && !(o_ptr->ident & IDENT_STORE) )
     {
